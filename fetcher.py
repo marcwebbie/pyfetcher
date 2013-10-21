543cebca09c3ae67eae60112f375a909e4222767
marcwebbie <marcwebbie@gmail.com>
2013-10-21 08:41:45 -0300

    Packaged project

--- fetcher.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import interface
-
-if __name__ == "__main__":
-    interface.Console.run()
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    raise ImportError("Install setuptools: pip install setuptools")
+
+setup(
+    name='PyFetcher',
+    version='0.0.1',
+    description='Python media streaming url fetcher',
+    author='Marc Webbie',
+    author_email='marcwebbie@gmail.com',
+    url='https://bitbucket.org/marcwebbie/pyfetcher/',
+    packages=['pyfetcher'],
+    scripts=['bin/pyfetcher'],
+    install_requires=[
+        'PyQuery',
+    ],
+)
