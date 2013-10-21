b6549e5ec9e5e19a123b0017dadc85b70c5b32b6
marcwebbie <marcwebbie@gmail.com>
2013-10-21 08:54:03 -0300

    Updated metadata

--- setup.py
+++ setup.py
@@ -5,15 +5,22 @@ try:
 except ImportError:
     raise ImportError("Install setuptools: pip install setuptools")
 
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
 setup(
     name='PyFetcher',
     version='0.0.1',
     description='Python media streaming url fetcher',
+    long_description=readme(),
     author='Marc Webbie',
     author_email='marcwebbie@gmail.com',
     url='https://bitbucket.org/marcwebbie/pyfetcher/',
     packages=['pyfetcher'],
     scripts=['bin/pyfetcher'],
+    license='MIT'
     install_requires=[
         'PyQuery',
     ],
