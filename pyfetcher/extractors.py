e368994efcb51ba2f8585b7fcdb73d352144debe
marcwebbie <marcwebbie@gmail.com>
2013-10-23 01:08:58 -0300

    Added wire unpacker and add divxstage extractor

--- pyfetcher/extractors.py
+++ pyfetcher/extractors.py
@@ -13,9 +13,122 @@ except:
     str = unicode
     range = xrange
 
-from pyquery import PyQuery as pq
+from pyquery import PyQuery
 
-DEBUG = False
+DEBUG = True
+
+
+class Unpacker(object):
+
+    """
+    Unpacker is a base class to unpack various function based eval packed functions writen in javascript
+    """
+    param_regex = None
+
+    def unpack():
+        raise NotImplemented('unpack method is not implemented')
+
+
+class WiseUnpacker(Unpacker):
+    param_regex = re.compile(
+        r";\}\('(?P<param_w>\w+)'[\s,]+'(?P<param_i>\w+)'[\s,]+'(?P<param_s>\w+)'[\s,]+'(?P<param_e>\w+)'\)\);")
+
+    @staticmethod
+    def unpack(w, i, s, e):
+        """
+        function (w, i, s, e) {
+          var lIll = 0;
+          var ll1I = 0;
+          var Il1l = 0;
+          var ll1l = [];
+          var l1lI = [];
+          while (true) {
+              if (lIll < 5) l1lI.push(w.charAt(lIll));
+              else if (lIll < w.length) ll1l.push(w.charAt(lIll));
+              lIll++;
+              if (ll1I < 5) l1lI.push(i.charAt(ll1I));
+              else if (ll1I < i.length) ll1l.push(i.charAt(ll1I));
+              ll1I++;
+              if (Il1l < 5) l1lI.push(s.charAt(Il1l));
+              else if (Il1l < s.length) ll1l.push(s.charAt(Il1l));
+              Il1l++;
+              if (w.length + i.length + s.length + e.length == ll1l.length + l1lI.length + e.length) break;
+          }
+          var lI1l = ll1l.join('');
+          var I1lI = l1lI.join('');
+          ll1I = 0;
+          var l1ll = [];
+          for (lIll = 0; lIll < ll1l.length; lIll += 2) {
+              var ll11 = -1;
+              if (I1lI.charCodeAt(ll1I) % 2) ll11 = 1;
+              l1ll.push(String.fromCharCode(parseInt(lI1l.substr(lIll, 2), 36) - ll11));
+              ll1I++;
+              if (ll1I >= l1lI.length) ll1I = 0;
+          }
+          return l1ll.join('');
+        }
+        """
+
+        va = 0
+        vb = 0
+        vc = 0
+        vd = []
+        ve = []
+        while True:
+            if va < 5:
+                ve.append(w[va])
+            elif va < len(w):
+                vd.append(w[va])
+
+            va += 1
+
+            if vb < 5:
+                ve.append(i[vb])
+            elif vb < len(i):
+                vd.append(i[vb])
+
+            vb += 1
+
+            if vc < 5:
+                ve.append(s[vc])
+            elif vc < len(s):
+                vd.append(s[vc])
+
+            vc += 1
+
+            if (len(w) + len(i) + len(s) + len(e)) == (len(vd) + len(ve) + len(e)):
+                break
+
+        vf = "".join(vd)  # vf = vd.join('')
+        vg = "".join(ve)  # vg = ve.join('')
+
+        vb = 0
+        vi = []
+        for va in range(0, len(vd), 2):  # (va = 0; va < vd; va += 2)
+            vj = -1
+            if ord(vg[vb]) % 2:  # (vg.charCodeAt(vb) % 2):
+                vj = 1
+
+            # vi.append(String.fromCharCode(parseInt(vf.substr(va, 2), 36) - vj))
+            vi.append(chr(int(vf[va:va + 2], 36) - vj))
+
+            vb += 1
+
+            if vb >= len(ve):
+                vb = 0
+
+        result = ''.join(vi)  # vi.join('')
+
+        rgx = WiseUnpacker.param_regex
+        if re.search(rgx, result):
+            w = re.search(rgx, result).group('param_w')
+            i = re.search(rgx, result).group('param_i')
+            s = re.search(rgx, result).group('param_s')
+            e = re.search(rgx, result).group('param_e')
+
+            result = WiseUnpacker.unpack(w, i, s, e)
+
+        return result
 
 
 class BaseExtractor(object):
@@ -115,9 +228,9 @@ class NowVideoExtractor(BaseExtractor):
         e.style.visibility = '';
 
 
-    ######################################
+    # 
     info for extracting
-    ######################################
+    # 
     When a page with a videonow video is request:
     + video now looks for uses the script above to find "key" and "file"
     + uses "key" and "file" to build a request to php script at "/api/player.api.php" with query params
@@ -170,9 +283,6 @@ class NowVideoExtractor(BaseExtractor):
 
         # build api request url with params to api
         api_url = "http://www.nowvideo.sx/api/player.api.php?{}".format(urlencode(qparams))
-
-        # import pdb
-        # pdb.set_trace()
 
         # fetch response with containing raw url
         html_response = str(self.fetch_page(api_url))
@@ -239,8 +349,8 @@ class VidbullExtractor(BaseExtractor):
         html_embed = str(urlopen(dest_url).read())
 
         # get script with the packed eval function
-        d = pq(html_embed)
-        script_text_raw = d('#player_code script:not([src])').text()
+        pq = PyQuery(html_embed)
+        script_text_raw = pq('#player_code script:not([src])').text()
         script_text = re.sub(r'\\', '', str(script_text_raw))
 
         rgx = re.compile(r"}\('(.+)',(\d+),(\d+),'([\w|]+)'")
@@ -273,10 +383,86 @@ class VidbullExtractor(BaseExtractor):
         pass
 
 
+class DivxStageExtractor(BaseExtractor):
+
+    """
+    http://www.divxstage.eu/ to videonow
+
+    example url: http://embed.divxstage.eu/embed.php?v=http://www.divxstage.eu/file/b04c4b011a81e&height=438&width=653
+
+    find "key" and "file" query parameters
+    example where found: <param name="flashvars" value="width=1920&amp;height=507&amp;domain=http://www.divxstage.eu&amp;file=httpwwwdivxstageeufileb04c4b011a81e&amp;filekey=177.206.184.253-94dc94e31c032d37fc9c1c6b12e7ae51&amp;advURL=http://www.divxstage.eu/video/httpwwwdivxstageeufileb04c4b011a81e">
+
+    build api url from
+    http://www.divxstage.eu/api/player.api.php?
+    example: http://www.divxstage.eu/api/player.api.php?key=177%2E206%2E184%2E253-94dc94e31c032d37fc9c1c6b12e7ae51&file=httpwwwdivxstageeufileb04c4b011a81e
+    
+    return is a string with where url to video can be found
+    example: url=http://t40.divxstage.eu/dl/cdf205451564ca45e342fbba2bf15b80/52662d51/ff58324d2c9602ce199e769dfa2b3296fc.flv&title=revenge.302.hdtvlolwso%26asdasdas&site_url=http://www.divxstage.eu/video/270b511691068&seekparm=&enablelimit=0
+    """
+
+    def __init__(self):
+        self.host_list = ["divxstage.eu"]
+        self.holder_url = "http://embed.divxstage.eu/embed.php?&width=653&height=438&v={}"
+        self.regex_url = None
+        # super().__init__(self)
+        super(DivxStageExtractor, self).__init__()
+
+    def raw_url(self, video_id):
+        dest_url = self.holder_url.format(video_id)
+        html_embed = self.fetch_page(dest_url)
+
+        pq = PyQuery(html_embed)
+        scripts_text = pq('body script').text()
+
+        rgx = WiseUnpacker.param_regex
+
+        # unpack script
+        unpacked_script = None
+        if re.search(rgx, scripts_text):
+            # find unpack function params
+
+            w = re.search(rgx, scripts_text).group('param_w')
+            i = re.search(rgx, scripts_text).group('param_i')
+            s = re.search(rgx, scripts_text).group('param_s')
+            e = re.search(rgx, scripts_text).group('param_e')
+
+            unpacked_script = WiseUnpacker.unpack(w, i, s, e)
+
+        # find "key" param
+        key_var_rgx = re.search(r'filekey=(\w+)', unpacked_script).group(1)
+        key_param = re.search(key_var_rgx + r'="([\w\.\-]+)"', unpacked_script).group(1)
+        # key_param = re.search(r'll="([\w\.\-]+)', unpacked_script).group(1)
+
+        # find "file" param
+        file_param = re.search(
+            r'advURL="http://www\.divxstage\.eu/video/(\w+)"', unpacked_script).group(1)
+
+        qparams = {
+            "key": key_param,
+            "file": file_param,
+        }
+
+        # call api to get result string containing raw url
+        api_call = "http://www.divxstage.eu/api/player.api.php?" + urlencode(qparams)
+        api_result = str(self.fetch_page(api_call))
+
+        # extract raw url from api_call result
+        url_found = None
+        try:
+            rgx = re.compile(r'url=(?P<rurl>http://[\w\.\-/&=?]+\.flv|mp4|avi|mk4|m4a)')
+            url_found = rgx.search(api_result).group('rurl')
+        except (IndexError, AttributeError):
+            if DEBUG:
+                print('url was not found in response: {}'.format(api_result))
+
+        return(url_found)
+
 EXTRACTOR_INSTANCES = (
     GorillaVidExtractor(),
     VidbullExtractor(),
     NowVideoExtractor(),
+    DivxStageExtractor(),
 )
 
 
@@ -294,3 +480,7 @@ def get_by_hostname(hostname):
     for extractor in get_instances():
         if extractor.is_valid_host(hostname):
             return extractor
+
+if __name__ == '__main__':
+    extor = DivxStageExtractor()
+    extor.raw_url('b04c4b011a81e')
