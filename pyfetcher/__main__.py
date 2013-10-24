90fb665caca9f1d39bc282cfe6771aafdf648dac
marcwebbie <marcwebbie@gmail.com>
2013-10-24 18:29:58 -0300

    fixed import error

--- pyfetcher/__main__.py
+++ pyfetcher/__main__.py
@@ -10,6 +10,7 @@ if __package__ is None and not hasattr(sys, "frozen"):
     # direct call of __main__.py
     import os.path
     path = os.path.realpath(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(path))
     sys.path.append(os.path.dirname(os.path.dirname(path)))
 
 
